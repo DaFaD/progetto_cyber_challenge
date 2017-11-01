d3ffe0343b8c98425264024b692f6110a203cdab
DanieleSinibaldi <daniele.sinibaldi@hotmail.it>
2017-11-01 14:07:00 +0100

    Aggiunti test per le validazioni delle email degli utenti studente

--- app/models/user_studente.rb
+++ app/models/user_studente.rb
@@ -1,7 +1,8 @@
 class UserStudente < ActiveRecord::Base
     validates :name, presence: true, length: { maximum: 50 }
     validates :surname, presence: true, length: { maximum: 50 }
-    validates :email, presence: true, length: { maximum: 255 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@(gmail.it|gmail.com|studenti.uniroma1.it)\z/i # /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
     validates :username, presence: true, length: { maximum: 50 }
     validates :fiscalCode, presence: true, length: { maximum: 16, minimum: 16 }
     validates :birthDay, presence: true
--- test/models/user_studente_test.rb
+++ test/models/user_studente_test.rb
@@ -6,7 +6,7 @@ class UserStudenteTest < ActiveSupport::TestCase
   # end
   
   def setup
-    @userStudente = UserStudente.new(name: "daniele", surname: "Sinibaldi", email: "daniele.sinibaldi@hotmail.it", username: "Daniel46-95", fiscalCode: "SNBDNL95H20C858U", birthDay: Date.new(1995, 6, 20))
+    @userStudente = UserStudente.new(name: "daniele", surname: "Sinibaldi", email: "daniel46.95@gmail.com", username: "Daniel46-95", fiscalCode: "SNBDNL95H20C858U", birthDay: Date.new(1995, 6, 20))
   end
   
   test "should be valid" do
@@ -71,5 +71,21 @@ class UserStudenteTest < ActiveSupport::TestCase
   test "fiscal code should not be shorter than 16 char" do
     @userStudente.fiscalCode = "a" * 15
     assert_not @userStudente.valid?
+  end
+  
+  test "email validation should accept valid addresses" do
+    valid_addresses = %w[nome.85-92@gmail.com utente@gmail.it utente@gmail.com cognome.1749374@studenti.uniroma1.it]
+    valid_addresses.each do |valid_address|
+        @userStudente.email = valid_address
+        assert @userStudente.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+  
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example. foo@bar_baz.com foo@bar+baz.com utente@hotmail.it utente@live.com nome.cognome@alice.com]
+    invalid_addresses.each do |invalid_address|
+        @userStudente.email = invalid_address
+        assert_not @userStudente.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 end
