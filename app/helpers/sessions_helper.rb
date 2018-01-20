065a20536d3d5d7923c7dbf293a225feac1bcde1
DanieleSinibaldi <daniele.sinibaldi@hotmail.it>
2018-01-20 12:36:57 +0100

    Modificata la navbar in modo tale che i suoi tasti cambino a seconda se si è loggati o no e a seconda del tipo di utente che si è loggato

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,15 +2,49 @@ module SessionsHelper
     # Logs in the given user.
     def log_in(user)
         session[:user_id] = user.id
+        session[:user_type_string] = user.class.to_s
     end
     
     # Returns the current logged-in user (if any).
     def current_user
-        @current_user ||= UserAdmin.find_by(id: session[:user_id]) || UserProfessore.find_by(id: session[:user_id]) || UserStudente.find_by(id: session[:user_id])
+        if @current_user != nil
+            @current_user
+        elsif session[:user_type_string] == "UserAdmin"
+            @current_user = UserAdmin.find_by(id: session[:user_id])
+        elsif session[:user_type_string] == "UserProfessore"
+            @current_user = UserProfessore.find_by(id: session[:user_id])
+        elsif session[:user_type_string] == "UserStudente"
+            @current_user = UserStudente.find_by(id: session[:user_id])
+        else
+            nil
+        end
     end
     
     # Returns true if the user is logged in, false otherwise.
     def logged_in?
         !current_user.nil?
+    end
+    
+    # Returns "nil" if current_user== nil , otherwise returns a string that shows the logged user's type
+    def current_user_type_to_s
+        stringa_res= ""
+        if current_user == nil
+            stringa_res= "nil"
+        else
+            stringa_res= current_user.class.to_s
+        end
+        stringa_res
+    end
+    
+    def user_admin?
+        current_user_type_to_s == "UserAdmin"
+    end
+    
+    def user_professore?
+        current_user_type_to_s == "UserProfessore"
+    end
+    
+    def user_studente?
+        current_user_type_to_s == "UserStudente"
     end
 end
