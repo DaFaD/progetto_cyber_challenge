18b4bfb22404a5f66d7ad85ec9c23db8fa6060c0
FaPa95 <fabio.pacileo@gmail.com>
2018-01-29 09:52:36 +0100

    adjustment question-model structure

--- db/migrate/20180129085149_create_questions.rb
+++ db/migrate/20180129085149_create_questions.rb
@@ -1,11 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
-      t.string :ans1
-      t.string :ans2
-      t.string :ans3
-      t.string :ans4
-      t.string :ansOk
+      t.text :Text
+      t.text :Ans1
+      t.text :Ans2
+      t.text :Ans3
+      t.text :Ans4
+      t.string :Ans_ok
 
       t.timestamps null: false
     end
