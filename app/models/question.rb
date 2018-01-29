646b93869bea5874dba3fac04f9f7f02e1bdb12f
FaPa95 <fabio.pacileo@gmail.com>
2018-01-29 10:15:40 +0100

    deleted Quiz model

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,8 @@
 class Question < ActiveRecord::Base
+    validates :text, presence: true
+    validates :ans1, presence: true
+    validates :ans2, presence: true
+    validates :ans3, presence: true
+    validates :ans4, presence: true
+    validates :ans_ok, presence: true
 end
--- db/migrate/20180129084024_create_quizzes.rb
+++
@@ -1,9 +0,0 @@
-class CreateQuizzes < ActiveRecord::Migration
-  def change
-    create_table :quizzes do |t|
-      t.array{40} :Questions
-
-      t.timestamps null: false
-    end
-  end
-end
